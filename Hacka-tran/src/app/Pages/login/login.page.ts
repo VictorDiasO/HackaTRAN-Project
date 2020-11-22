6ed15eb8b2a1ec57eed36aea239a7b65f0ac8e71
Breno Fachini <breno.r.fachini@accenture.com>
2020-11-22 16:52:54 -0300

    adding login form for validator

--- Hacka-tran/src/app/Pages/login/login.page.ts
+++ Hacka-tran/src/app/Pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  private loginFG: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.loginFG = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit() {
   }
