516201f4c0841aa0b7088aa6d1716b21d9e51f5d
Breno Fachini <breno.r.fachini@accenture.com>
2020-11-22 11:31:44 -0300

    init first generated verswion

--- android/app/src/main/kotlin/com/example/hackatran_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hackatran_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,176 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapp/voudebikeapp/generatedsignupconfirmationwidget/GeneratedSignUpConfirmationWidget.dart';
+import 'package:flutterapp/voudebikeapp/generatedquizwidget/GeneratedQuizWidget.dart';
+import 'package:flutterapp/voudebikeapp/generatedquizwidget2/GeneratedQuizWidget2.dart';
+import 'package:flutterapp/voudebikeapp/generatedsignupwidget/GeneratedSignUpWidget.dart';
+import 'package:flutterapp/voudebikeapp/generatediphone11prox2widget/GeneratedIPhone11ProX2Widget.dart';
+import 'package:flutterapp/voudebikeapp/generatediphone11prox4widget/GeneratedIPhone11ProX4Widget.dart';
+import 'package:flutterapp/voudebikeapp/generatediphone11prox5widget/GeneratedIPhone11ProX5Widget.dart';
+import 'package:flutterapp/voudebikeapp/generatediphone11prox3widget/GeneratedIPhone11ProX3Widget.dart';
+import 'package:flutterapp/voudebikeapp/generatediphone11prox6widget/GeneratedIPhone11ProX6Widget.dart';
+import 'package:flutterapp/voudebikeapp/generatedmapawidget/GeneratedMapaWidget.dart';
+import 'package:flutterapp/voudebikeapp/generatedbotoreportarwidget/GeneratedBotoreportarWidget.dart';
+import 'package:flutterapp/voudebikeapp/generatedmapawidget1/GeneratedMapaWidget1.dart';
+import 'package:flutterapp/voudebikeapp/generatedmapawidget2/GeneratedMapaWidget2.dart';
+import 'package:flutterapp/voudebikeapp/generatedestatsticaswidget/GeneratedEstatsticasWidget.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(VouDeBikeApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class VouDeBikeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'HackaTRAN App',
+      title: 'Flutter Demo',
       theme: ThemeData(
-          fontFamily: "Poppins",
-          textTheme: TextTheme(
-            body1: TextStyle(color: Colors.black26),
-          )),
-      home: HomeScreeen(),
-    );
-  }
-}
-
-class HomeScreeen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          // Bglh azul do fundo
-          ClipPath(
-              clipper: MyClipper(),
-              child: Container(
-                padding: EdgeInsets.only(left: 40, top: 50, right: 20),
-                height: 300,
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Align(
-                      alignment: Alignment.topRight,
-                    ),
-                    SizedBox(height: 20),
-                    Expanded(
-                      child: Stack(
-                        children: <Widget>[
-                          Image.asset("assets/images/voudebikeLogo.png",
-                              width: 200,
-                              fit: BoxFit.fitWidth,
-                              alignment: Alignment.topCenter),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              )),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 70),
-            height: 60,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: Colors.blue[600],
-                borderRadius: BorderRadius.circular(25),
-                border: Border.all(
-                  color: Color(0xFFE5E5E5),
-                )),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                FlatButton(
-                  onPressed: () {
-                    /*...*/
-                  },
-                  child: Text("Entrar",
-                      style: TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.w600,
-                              height: 2)
-                          .copyWith(color: Colors.white)),
-                ),
-              ],
-            ),
-          ),
-
-          // Space between buttons
-          ClipPath(
-              clipper: MyClipper(),
-              child: Container(
-                padding: EdgeInsets.only(left: 40, top: 25, right: 20),
-                height: 15,
-                width: double.infinity,
-              )),
-
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 70),
-            height: 60,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: Colors.blue[600],
-                borderRadius: BorderRadius.circular(25),
-                border: Border.all(
-                  color: Color(0xFFE5E5E5),
-                )),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                FlatButton(
-                  onPressed: () {
-                    /*...*/
-                  },
-                  child: Text("Cadastrar",
-                      style: TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.w600,
-                              height: 2)
-                          .copyWith(color: Colors.white)),
-                ),
-              ],
-            ),
-          ),
-
-          ClipPath(
-              clipper: MyClipper(),
-              child: Container(
-                padding: EdgeInsets.only(left: 40, top: 20, right: 20),
-                height: 190,
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Align(
-                      alignment: Alignment.topRight,
-                    ),
-                    SizedBox(height: 2),
-                    Expanded(
-                      child: Stack(
-                        children: <Widget>[
-                          Positioned(
-                              top: 5,
-                              left: 40,
-                              right: 40,
-                              child: Text(
-                                  "O custo de manutenção de uma bicicleta é 20 vezes menor que o de um veículo automotor.",
-                                  style: TextStyle(
-                                          fontSize: 19,
-                                          fontWeight: FontWeight.w600)
-                                      .copyWith(color: Colors.black))),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              )),
-        ],
+        primarySwatch: Colors.blue,
       ),
+      initialRoute: '/GeneratedSignUpWidget',
+      routes: {
+        '/GeneratedSignUpConfirmationWidget': (context) =>
+            GeneratedSignUpConfirmationWidget(),
+        '/GeneratedQuizWidget': (context) => GeneratedQuizWidget(),
+        '/GeneratedQuizWidget2': (context) => GeneratedQuizWidget2(),
+        '/GeneratedSignUpWidget': (context) => GeneratedSignUpWidget(),
+        '/GeneratedIPhone11ProX2Widget': (context) =>
+            GeneratedIPhone11ProX2Widget(),
+        '/GeneratedIPhone11ProX4Widget': (context) =>
+            GeneratedIPhone11ProX4Widget(),
+        '/GeneratedIPhone11ProX5Widget': (context) =>
+            GeneratedIPhone11ProX5Widget(),
+        '/GeneratedIPhone11ProX3Widget': (context) =>
+            GeneratedIPhone11ProX3Widget(),
+        '/GeneratedIPhone11ProX6Widget': (context) =>
+            GeneratedIPhone11ProX6Widget(),
+        '/GeneratedMapaWidget': (context) => GeneratedMapaWidget(),
+        '/GeneratedBotoreportarWidget': (context) =>
+            GeneratedBotoreportarWidget(),
+        '/GeneratedMapaWidget1': (context) => GeneratedMapaWidget1(),
+        '/GeneratedMapaWidget2': (context) => GeneratedMapaWidget2(),
+        '/GeneratedEstatsticasWidget': (context) =>
+            GeneratedEstatsticasWidget(),
+      },
     );
-  }
-}
-
-class MyClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    path.lineTo(0, size.height - 80);
-    path.quadraticBezierTo(
-        size.width / 2, size.height, size.width, size.height - 80);
-    path.lineTo(size.width, 0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false;
   }
 }
