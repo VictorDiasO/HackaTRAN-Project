435a7c1a9a9294a42b7a85c93506f86e5f222d2c
Breno Fachini <breno.r.fachini@accenture.com>
2020-11-22 18:53:18 -0300

    changing signUp method to sms

--- Hacka-tran/src/app/Pages/sign-up/sign-up.page.ts
+++ Hacka-tran/src/app/Pages/sign-up/sign-up.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
+import * as firebase from "firebase/";
 
 @Component({
   selector: 'app-sign-up',
@@ -10,7 +13,7 @@ export class SignUpPage implements OnInit {
 
   private signUpFG: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { 
+  constructor(private formBuilder: FormBuilder, private fireAuth: AngularFireAuth) { 
     this.signUpFG = this.formBuilder.group({
       name: ['', Validators.required],
       surename: ['', Validators.required],
@@ -25,6 +28,33 @@ export class SignUpPage implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  getMaxDate(){
+    return moment().format('YYYY-MM-DD');
+  }
+
+  async doSignUp(){
+    const recaptchaVerifier: firebase.default.auth.RecaptchaVerifier = new firebase.default.auth.RecaptchaVerifier('recapcha', {
+      size: 'invisible',
+      callback: (response: any) => {
+        console.log(response);
+      }
+    })
+    let confirmation: firebase.default.auth.ConfirmationResult;
+    try{
+      await this.fireAuth.signInWithPhoneNumber('+5511957198000', recaptchaVerifier);
+    } catch(error) {
+      confirmation = null;
+    }
+    if(confirmation != null){
+      console.log(confirmation);
+      try{
+        await confirmation.confirm('1234');
+      } catch(error){
+        console.log(error);
+      }
+    }
   }
 
 }
