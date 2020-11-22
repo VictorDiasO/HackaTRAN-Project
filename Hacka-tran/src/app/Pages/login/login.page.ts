170cf6cbbafd36981f2af98c5f3c1620b8d0bc08
Breno Fachini <breno.r.fachini@accenture.com>
2020-11-22 17:19:04 -0300

    login with firebase

--- Hacka-tran/src/app/Pages/login/login.page.ts
+++ Hacka-tran/src/app/Pages/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +11,10 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class LoginPage implements OnInit {
 
   private loginFG: FormGroup;
+  private invalidLogin: boolean;
 
-  constructor(private formBuilder: FormBuilder) { 
+  constructor(private formBuilder: FormBuilder, private fireAuth: AngularFireAuth, private router: Router) { 
+    this.invalidLogin = false;
     this.loginFG = this.formBuilder.group({
       email: ['', Validators.required],
       password: ['', Validators.required],
@@ -21,7 +25,12 @@ export class LoginPage implements OnInit {
   }
 
   async doLogin(){
-    console.log('login done');
+    try{
+      await this.fireAuth.signInWithEmailAndPassword(this.loginFG.get('email').value, this.loginFG.get('password').value);
+      this.router.navigate(['home']);
+    } catch(error){
+      this.invalidLogin = true;
+    }
   }
 
 }
